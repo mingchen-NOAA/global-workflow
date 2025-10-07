22b2fac820c41301f4ebaef1cb4999b43842c5cf
Ming Chen <ming.chen@clogin08.cactus.wcoss2.ncep.noaa.gov>
2025-10-07 18:03:50 +0000

    fix g2lib/g2clib warnings in wavepostsbs log

--- ush/wave_grib2_sbs.sh
+++ ush/wave_grib2_sbs.sh
@@ -100,11 +100,11 @@ if [[ ! -s gribfile ]]; then
 fi
 
 if [[ ${fhr} -gt 0 ]]; then
-  ${WGRIB2} gribfile -set_date "${PDY}${cyc}" -set_ftime "${fhr} hour fcst" -grib "${outfile}"
+  ${WGRIB2} -g2clib 0 gribfile -set_date "${PDY}${cyc}" -set_ftime "${fhr} hour fcst" -set_grib_type simple -grib "${outfile}"
   err=$?
 else
-  ${WGRIB2} gribfile -set_date "${PDY}${cyc}" -set_ftime "${fhr} hour fcst" \
-    -set table_1.4 1 -set table_1.2 1 -grib "${outfile}"
+  ${WGRIB2} -g2clib 0 gribfile -set_date "${PDY}${cyc}" -set_ftime "${fhr} hour fcst" \
+    -set table_1.4 1 -set table_1.2 1 -set_grib_type simple -grib "${outfile}"
   err=$?
 fi
 
